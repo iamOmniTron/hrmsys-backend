05f690f4a6398c911425d50d76ccc99732e855ad
musa abdulmumeen <abdulmumeen15500@gmail.com>
2022-04-26 00:54:45 +0100

    chore: added more routes

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,9 @@ const {Router} = require("express");
 const {Login} = require("../controller/admin");
 const {auth,requireAdminAccess} = require("../middleware/auth");
 const {addEmployee,editEmployee,getEmployee,getEmployees,deleteEmployee} = require("../controller/employee");
+const {addProfession,editProfession,getProfession,getProfessions,deleteProfession} = require("../controller/profession");
+const {addSkill,editSkill,getSkill,getSkills,deleteSkill} = require("../controller/vocation");
+const {getSalaries} = require("../controller/salary");
 
 
 const router = Router();
@@ -10,10 +13,27 @@ const router = Router();
 router.post("/employee",auth,requireAdminAccess,addEmployee);
 router.get("/employee/:employeeId",auth,requireAdminAccess,getEmployee);
 router.post("/employee/:employeeId",auth,requireAdminAccess,editEmployee);
-router.get("/employee/all",auth,requireAdminAccess,getEmployees);
+router.get("/employees/all",auth,requireAdminAccess,getEmployees);
 router.delete("/employee/:employeeId",auth,requireAdminAccess,deleteEmployee);
 
 // AUTH ROUTE
 router.post("/admin/login",Login);
+
+// PROFESSION
+router.post("/profession",auth,requireAdminAccess,addProfession);
+router.get("/profession/:professionId",auth,requireAdminAccess,getProfession);
+router.post("/profession/:professionId",auth,requireAdminAccess,editProfession);
+router.get("/professions/all",auth,requireAdminAccess,getProfessions);
+router.delete("/profession/:professionId",auth,requireAdminAccess,deleteProfession);
+
+// VOCATION
+router.post("/skill",auth,requireAdminAccess,addSkill);
+router.get("/skill/:skillId",auth,requireAdminAccess,getSkill);
+router.post("/skill/:skillId",auth,requireAdminAccess,editSkill);
+router.get("/skills/all",auth,requireAdminAccess,getSkills);
+router.delete("/skill/:skillId",auth,requireAdminAccess,deleteSkill);
+
+// SALARY
+router.get("/salaries/all",auth,requireAdminAccess,getSalaries);
 
 module.exports = router;

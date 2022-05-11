72c980bd48e4d963ce36aff815dd77aece95ec15
musa abdulmumeen <abdulmumeen15500@gmail.com>
2022-05-11 10:51:48 +0100

    changes

--- routes/index.js
+++ routes/index.js
@@ -20,7 +20,7 @@ router.post("/auth/employee/login",login);
 router.get("/auth/employee/:email",getUrls);
 router.post("employee/training/:trainingId",auth,joinTraining);
 router.get("/employees/all",auth,requireAdminAccess,getEmployees);
-router.delete("/employee/:employeeId",auth,requireAdminAccess,deleteEmployee);
+router.post("/delete/employee/:employeeId",auth,requireAdminAccess,deleteEmployee);
 router.get("/employees/salary",auth,requireAdminAccess,getPayrolls);
 
 
@@ -32,14 +32,14 @@ router.post("/profession",auth,requireAdminAccess,addProfession);
 router.get("/profession/:professionId",auth,requireAdminAccess,getProfession);
 router.post("/profession/:professionId",auth,requireAdminAccess,editProfession);
 router.get("/professions/all",auth,requireAdminAccess,getProfessions);
-router.delete("/profession/:professionId",auth,requireAdminAccess,deleteProfession);
+router.post("/delete/profession/:professionId",auth,requireAdminAccess,deleteProfession);
 
 // TRAINING
 router.post("/training",auth,requireAdminAccess,addTraining);
 router.get("/training/:trainingId",auth,requireAdminAccess,getTraining);
 router.get("/trainings/all",auth,getTrainings);
 router.post("/training/:trainingId",auth,requireAdminAccess,editTraining);
-router.delete("/training/:trainingId",auth,requireAdminAccess,deleteTraining);
+router.post("/delete/training/:trainingId",auth,requireAdminAccess,deleteTraining);
 
 // PAYROLL
 router.post("/pay/employee",auth,requireAdminAccess,payEmployee);
